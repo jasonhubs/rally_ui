e089454b77b25dd1780de0ff610d97b8c4c9d716
scbhooshan <scbhooshan@gmail.com>
2015-02-24 12:22:25 -0800

    adasd

--- rally_dashboard/urls.py
+++ rally_dashboard/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, patterns
 from rally_dashboard import views
-
+from django.conf import settings
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
