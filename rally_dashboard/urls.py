c7284ee76f6215d2cdd2222c1ae24884c2624edc
scbhooshan <scbhooshan@gmail.com>
2015-02-24 12:14:27 -0800

    url

--- rally_dashboard/urls.py
+++ rally_dashboard/urls.py
@@ -3,4 +3,5 @@ from rally_dashboard import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
